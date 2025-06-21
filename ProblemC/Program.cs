9e2c817d10b2ce8a485e58c2941d809f288801be
Peter Briggs <peterjamesbriggs@gmail.com>
2025-06-21 20:10:20 +0200

    :sparkles: Read in number of test cases

--- ProblemC/Program.cs
+++ ProblemC/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Read input until we encounter 0
+        while (true)
+        {
+            string? input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input)) break;
+
+            int x = int.Parse(input);
+            if (x == 0) break;
+
+            Console.WriteLine(x);
+        }
+    }
+
+}
